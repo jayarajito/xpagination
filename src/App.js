b477449ee7b5cb9abfc4a18a40ef99cc50adf202
JAYARAJ <jayarajcs2011@hotmail.com>
2024-06-24 00:34:35 +0530

    alert fixedd

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ const App = () => {
     const fetchEmployees = async () => {
       try {
         const response = await axios.get(
-          "https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json1"
+          "https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json"
         ); // Replace with your API endpoint
         setEmployees(response.data);
       } catch (error) {
